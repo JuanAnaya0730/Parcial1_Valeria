1dbb80f9a526807048c59429e230135a3ca64a04
Juan Anaya <juan.anayar@udea.edu.co>
2021-04-20 17:45:57 -0500

    Se implemento una funcion que imprime el menu de opciones.

--- Valeria_Parcial1.py
+++ Valeria_Parcial1.py
@@ -1,7 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 20 17:13:18 2021
+'''
+        PARCIAL 1 BIOINGENIERIA
 
-@author: janay
-"""
+Valeria Giraldo Agudelo
+Grupo 2
 
+Universidad de Antioquia
+Medellin - Antioquia
+Abril 2021
+
+'''
+
+def Menu():
+    
+    print("Menu:\n",
+          "1. Ingresar informacion de una pipeta.\n",
+          "2. Consultar informacion de una pipeta.\n",
+          "3. Estadisticos\n",
+          "4. Salir.\n")
+    
+    print("1. Ingresar informacion de una pipeta.")
+    print("2. Consultar informacion de una pipeta.")
+    print("3. Estadisticos")
+    print("4. Salir.")    
+    
+Menu()
