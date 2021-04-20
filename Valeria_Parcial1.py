53172c0e27d4a0fcdaeebb6386f5ba2917af7aa0
Juan Anaya <juan.anayar@udea.edu.co>
2021-04-20 18:31:25 -0500

    Se implementaron lineas de codigo que verifican si la opcion ingresada por el usuario es valida.

--- Valeria_Parcial1.py
+++ Valeria_Parcial1.py
@@ -11,16 +11,50 @@ Abril 2021
 '''
 
 def Menu():
-    
+    # Esta funcion se encarga de imprimir un menu de opciones
     print("Menu:\n",
           "1. Ingresar informacion de una pipeta.\n",
           "2. Consultar informacion de una pipeta.\n",
-          "3. Estadisticos\n",
-          "4. Salir.\n")
+          "3. Estadisticos.\n",
+          "4. Salir.")
+
+while True:
     
-    print("1. Ingresar informacion de una pipeta.")
-    print("2. Consultar informacion de una pipeta.")
-    print("3. Estadisticos")
-    print("4. Salir.")    
+    Menu() # Se imprime en consola el menu de opciones
+    option = input("Opcion: ") # option es la variable que almacenara la opcion escogida por el usuario
     
-Menu()
+    # Este ciclo se encarga de verficar si la opcion escogida por el usuario es valida
+    while(option.isnumeric() == False or int(option)<1 or int(option)>4):
+        print("Opcion invalida.\n") # Se advierte al usuario que la opcion ingresada es invalida
+        Menu() # Se imprime en consola el menu de opciones
+        option = input("Opcion: ") # Se le pide al usuario que ingrese una nueva opcion
+       
+    if(int(option) == 1):
+        print("El usuario debe ingresar informacion de una pipeta.")
+        
+    elif(int(option) == 2):
+        print("El usuario va a consultar informacion.")
+    
+    elif(int(option) == 3):
+        print("El usuario quiere ver estadisticas.")
+    
+    elif(int(option) == 4):
+        print("El usuario quiere salir.")
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
